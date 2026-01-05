4df44b3f7e236c701de43456fc23890afccc6bbe
RidhoFahrizal <farizalridho@gmail.com>
2026-01-05 11:26:26 +0700

    Material(5-1-2026):Mempelajari Materi 7

---
+++ 7_Constant/main.go
@@ -0,0 +1,19 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	const (
+		firstName string = "jack"
+		lastName string = "Whilshare"
+		value int32 = 8000000
+	)
+
+	fmt.Println("firstName",firstName)
+	fmt.Println("lastName", lastName)
+	fmt.Println(value)
+
+}
