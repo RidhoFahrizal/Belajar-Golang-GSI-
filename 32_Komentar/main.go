602be5f7e49e7a7b9043c4babf6af7c123fa7584
RidhoFahrizal <farizalridho@gmail.com>
2026-01-06 14:46:48 +0700

    Material(6-1-2026):Mempelajari Materi 32, komentar

---
+++ 32_Komentar/main.go
@@ -0,0 +1,27 @@
+package main 
+
+import(
+	"fmt"
+)
+
+
+func main(){
+	fmt.Println("ini adalah penggunaan comentar block")
+	/**
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	foo
+	*/
+
+	fmt.Println("Ini adalah Commentar untuk singgle line")
+	// ffoo foo foo foo foo foo foo foo foo foo
+
+}
